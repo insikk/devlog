36e45430208d834e791240825b5a8faf79134cae
insikk <insik92@gmail.com>
2022-03-18 22:19:12 +0900

    initial setup

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,4 +1,5 @@
 const headerNavLinks = [
+  // TODO: 접속자 locale 에 따라 다국어 지원하면 좋겠다. 한국에서 접속하면 한글로 보이도록...
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
--- data/projectsData.js
+++ data/projectsData.js
@@ -1,19 +1,23 @@
 const projectsData = [
   {
+    title: 'Dense Retrieval',
+    description: `Search experience powered by Deep Learning Models`,
+    imgSrc: '/static/images/naver.png',
+    href: 'https://www.naver.com',
+  },
+  {
+    title: 'Content Based Image Retrieval',
+    description: `스마트렌즈. Search with image. 글자로 검색하기 힘들면 이미지로 검색하세요.`,
+    imgSrc: '/static/images/naver.png',
+    href: 'https://tv.naver.com/v/2037414',
+  },
+  {
     title: 'A Search Engine',
     description: `What if you could look up any information in the world? Webpages, images, videos
     and more. Google has many features to help you find exactly what you're looking
     for.`,
     imgSrc: '/static/images/google.png',
     href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
   },
 ]
 
